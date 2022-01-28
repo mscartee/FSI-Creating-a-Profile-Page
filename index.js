157d33eec3e1b51df79cb3f37762207edab2a04c
Michael Cartee <admin@asgardcrolingai.lan>
2022-01-27 20:31:23 -0500

    midway through

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')
+header.setAttribute('class','dog-name')
+header.append("Rizzo")
+content.append(header)
+
+let divider=document.createElement('div')
+divider.setAttribute('class','dog-content')
+
+let dogImage =document.createElement('img')
+dogImage.setAttribute('class','dog-image')
+dogImage.src="./assets/rizzo.jpg"
+
+divider.append(dogImage)
+content.append(divider)
